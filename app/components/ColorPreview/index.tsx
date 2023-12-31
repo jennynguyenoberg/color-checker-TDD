6c15037547c41607618e3b0be1458fc541545373
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-12-31 15:48:08 +0100

    Update and add final styling

--- app/components/ColorPreview/index.tsx
+++ app/components/ColorPreview/index.tsx
@@ -5,8 +5,8 @@ interface ColorPreviewProps {
 
 const ColorPreview: React.FC<ColorPreviewProps> = ({ color1, color2 }) => {
   return (
-    <div style={{ color: color2, width: '50px', height: '50px' }}>
-      <div style={{ backgroundColor: color1, width: '50px', height: '50px' }}>
+    <div style={{ color: color2}}>
+      <div className='text-center flex items-center justify-center border-solid rounded-lg font-medium' style={{ backgroundColor: color1, width: '150px', height: '150px' }}>
         <p style={{ position: 'absolute' }}>Lorem ipsum</p>
       </div>
     </div>
