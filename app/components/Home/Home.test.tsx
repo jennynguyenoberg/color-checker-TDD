26915582916d2ee6f4e2057d8823ded7035224dd
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-12-31 12:09:18 +0100

    Change label names

--- app/components/Home/Home.test.tsx
+++ app/components/Home/Home.test.tsx
@@ -5,15 +5,15 @@ import Home from '.'
 describe('Home component', () => {
   test('updates color2 when ColorInput changes', () => {
     render(<Home />)
-    const colorInput2 = screen.getByLabelText('Color 2')
+    const colorInput2 = screen.getByLabelText('Text color')
     fireEvent.change(colorInput2, { target: { value: '#abcdef' } })
     expect(colorInput2).toHaveValue('#abcdef')
   })
 
   test('updates contrast result on color change', () => {
     render(<Home />)
-    const colorInput1 = screen.getByLabelText('Color 1')
-    const colorInput2 = screen.getByLabelText('Color 2')
+    const colorInput1 = screen.getByLabelText('Background color')
+    const colorInput2 = screen.getByLabelText('Text color')
     userEvent.type(colorInput1, '#123456')
     userEvent.type(colorInput2, '#abcdef')
     expect(screen.getByText(/contrast ratio/i)).toBeInTheDocument()
@@ -21,8 +21,8 @@ describe('Home component', () => {
 
   test('renders ColorInput components with correct labels', () => {
     render(<Home />)
-    const colorInput1 = screen.getByLabelText('Color 1')
-    const colorInput2 = screen.getByLabelText('Color 2')
+    const colorInput1 = screen.getByLabelText('Background color')
+    const colorInput2 = screen.getByLabelText('Text color')
     expect(colorInput1).toBeInTheDocument()
     expect(colorInput2).toBeInTheDocument()
   })
