6a515f39f384e31948a1cb37e2dd35d5a5fe8632
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-12-31 01:07:37 +0100

    Create Header component + move test-file into + add contents

--- app/components/Header/Header.test.tsx
+++ app/components/Header/Header.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Header from '../app/components/Header'
+import Header from '.'
 
 describe('Header Component', () => {
   test("renders header with text 'Color Contrast Checker'", () => {
---
+++ app/components/Header/index.tsx
@@ -0,0 +1,5 @@
+const Header: React.FC = () => {
+  return <h1>Color Contrast Checker</h1>
+}
+
+export default Header
