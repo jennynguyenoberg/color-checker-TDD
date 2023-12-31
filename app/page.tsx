36236fde55eada1c2dff0785a5f6ba260f89f71d
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-12-31 06:48:32 +0100

    Remove all components from main page and replace it with only the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-'use client'
-import ColorInput from './components/ColorInput'
-import ColorPreview from './components/ColorPreview'
-import ContrastResult from './components/ContrastResult'
-import Footer from './components/Footer'
-import Header from './components/Header'
+import Home from './components/Home'
 
 export default function Page() {
   return (
     <div>
-      <Header />
-      <ColorInput />
-      <ColorPreview />
-      <ContrastResult />
-      <Footer />
+      <Home />
     </div>
   )
 }
