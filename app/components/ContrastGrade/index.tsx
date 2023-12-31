816800408edc5860bafcdf195b1f2dd744354fcf
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-12-31 06:57:49 +0100

    Add functionality to render out grade value

--- app/components/ContrastGrade/index.tsx
+++ app/components/ContrastGrade/index.tsx
@@ -1,5 +1,9 @@
-const ContrastGrade: React.FC<ContrastGradeProps> = ({  }) => {
-  return <p>Grade: </p>
+interface ContrastGradeProps {
+  contrastGrade: string
+}
+
+const ContrastGrade: React.FC<ContrastGradeProps> = ({ contrastGrade }) => {
+  return <p>Grade: {contrastGrade}</p>
 }
 
 export default ContrastGrade
